9e640cccb3b739ba79ef032c5292a9c204cfa4dd
Nefari0uss <nnefari0uss@gmail.com>
2016-12-29 03:39:48 -0500

    i didn't really need to have ALL of these files...

--- vim/vim/templates/template.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-main(int argc, char *argv[])
-{
-	return 0;
-}
--- vim/vim/templates/template.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-"""
-Python source code - replace this with a description of the code and write the code below this text.
-"""
-
-
