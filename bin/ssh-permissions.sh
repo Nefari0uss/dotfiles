e7f359a58a3521a33bd26e2a0069033575e4f771
Nefari0uss <nnefari0uss@gmail.com>
2017-09-18 10:24:08 -0400

    fancy i3 lock, fixed ssh permissions, and urxvt help script

--- bin/ssh-permissions.sh
+++ bin/ssh-permissions.sh
@@ -2,7 +2,5 @@
 
 # Fix SSH "Permissions are too open" error.
 
-sudo chmod 600 ~/.ssh/id_rsa
-sudo chmod 600 ~/.ssh/id_rsa.pub
-sudo chmod 644 ~/.ssh/known_hosts
+sudo chmod 600 ~/.ssh/*
 sudo chmod 755 ~/.ssh
