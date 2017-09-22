bc357ccaedfa7cb5613884d2059ac84daffe5191
Nefari0uss <nnefari0uss@gmail.com>
2017-09-22 15:35:05 -0400

    files to have npm setup with the necessary/desired global installs w/o sudo access

---
+++ nodejs/npm-global-install.sh
@@ -0,0 +1,10 @@
+# Global NPM Installs
+# Nefari0uss
+
+# NPM installs that I use on every system globally. Consider using alongside the NPM local no-sudo script under the bin folder.
+npm i -g npm
+npm i -g yarn
+npm i -g @angular/cli
+npm i -g typescript
+npm i -g jshint
+npm i -g jpm
---
+++ nodejs/npm-install.sh
@@ -0,0 +1,5 @@
+# Setup NPM packages to install globally without sudo.
+./npm-g-nosudo.sh
+
+# Install the packages in this file "globally".
+./npm-global-install.sh
