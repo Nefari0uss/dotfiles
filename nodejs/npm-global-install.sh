0de4b8ad40277720e126e65297ab34f766bd1065
Nefari0uss <nnefari0uss@gmail.com>
2017-10-27 20:08:02 -0400

    moved .config stuff into config folder

--- nodejs/npm-global-install.sh
+++ nodejs/npm-global-install.sh
@@ -2,9 +2,37 @@
 # Nefari0uss
 
 # NPM installs that I use on every system globally. Consider using alongside the NPM local no-sudo script under the bin folder.
-npm i -g npm
-npm i -g yarn
-npm i -g @angular/cli
-npm i -g typescript
-npm i -g jshint
-npm i -g jpm
+npm i -g npm # Update NPM
+
+# Languages
+npm i -g typescript # Typescript Support
+npm i -g node-sass
+
+# Frameworks
+npm i -g @angular/cli # Angular CLI Tools
+
+# Linters
+npm i -g jshint # JS Static Code Analysis Tool
+npm i -g eslint # JS and JSX Linter
+npm i -g tslint # TypeScript Linter
+
+# Tools
+npm i -g web-ext # Mozilla Extension Dev Tools
+
+# Server
+npm i -g express # Express Server
+npm i -g express-generator # Express Project Generator
+npm i -g nodemon # Live reloading
+
+# Task Runners
+npm i -g gulp # Task Runner
+npm i -g grunt # Task Runner
+npm i -g gulp-cli
+
+# Database Support
+npm i -g mongodb
+
+# Fun
+npm i -g pokemon-terminal
+
+
