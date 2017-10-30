999ef19f0118c0a247301244ec6658bb13648b3d
Nefari0uss <nnefari0uss@gmail.com>
2017-10-30 12:14:40 -0400

    new brew packages and improve install process

--- macOS/install.sh
+++ macOS/install.sh
@@ -5,23 +5,33 @@
 # Set macOS preferences
 # ./macOS.sh
 
+
+# Accept Xcode License
+printf "Installing Xcode CLI tools...\n"
+xcode-select --install
+xcodebuild -license accept
+
 # Install HomeBrew
+printf "Installing Homebrew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Install Homebrew Cask and various packages
 brew update && brew doctor
+printf "Installing from Brewfile..."
 brew bundle
 
 # Make sure everything is good.
 brew cask doctor
 
-# Accept Xcode License
-xcodebuild -license accept
+printf "Adding prefix for manpages"
+$HOMEBREW_PREFIX/opt/coreutils/libexec/gnuman
 
 # Git LFS Install
+printf "Installing Git LFS"
 git lfs install 
 
 # Add iTerm2 shell integration.
+printf "Running iTerm2 integration."
 curl -L https://iterm2.com/misc/install_shell_integration_and_utilities.sh | bash
 
 # NPM (Global) Installs
