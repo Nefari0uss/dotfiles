f8108ee19ba3082d78cc81b151565b75aebb8488
Nefari0uss <nnefari0uss@gmail.com>
2018-03-27 18:56:53 -0400

    removed useless files from bin folder

--- bin/change-git-user.sh
+++
@@ -1,9 +0,0 @@
-- A config file per user that contains
---> User.Name
---> User.Email
---> GPG Signing Key Identifier
---> User Keyword or Identifying Name
-
-Bash script that will read the config file in the ~/.config/<project-name>/<config-files>
-and parse the user, email, and GPG identifier if it exists. Then it will set the git config options locally for the user.
-
