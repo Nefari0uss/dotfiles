48de7667f744f26970393f3a6c60524f6764195d
Nefari0uss <nnefari0uss@gmail.com>
2018-05-15 14:41:47 -0400

    npm packages to globally install

---
+++ nodejs/install_node_packages.sh
@@ -0,0 +1,8 @@
+
+@NPM_PACKAGES=(@angular-cli babel-cli caniuse-cmd diff-so-fancy ember-cli express
+eslint express-generator grunt gulp-cli ionic mklicense mongodb npm
+npm-check-updates pokemon-terminal
+tslint typescript vue-cli web-ext webpack yarn)
+
+npm i -g $PACKAGE
+
