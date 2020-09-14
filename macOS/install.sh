12e95765715e950e7f1b3db3a20869fe41b0029b
Nefari0uss <nnefari0uss@gmail.com>
2020-09-14 10:55:12 -0400

    update macOS install script

--- macOS/install.sh
+++ macOS/install.sh
@@ -36,25 +36,3 @@ git lfs install
 # Add iTerm2 shell integration.
 printf "Running iTerm2 integration."
 curl -L https://iterm2.com/misc/install_shell_integration_and_utilities.sh | bash
-
-# NPM (Global) Installs
-../nodejs/npm-install.sh
-
-# Install Ruby Gems
-# gem ../ruby/Gemfile
-
-# Install Python pip packages
-# python pip-file
-
-
-# Update Android SDK
-# android update sdk --no-ui
-
-# Set symblinks
-
-# Change somethings to be macOS only.
-# Maybe use sed?
-# ranger - change rc.conf to use iterm2 instead of w3m.
-# neofetch - change to use rc.conf instead of w3m.
-
-../bin/invaders.sh
