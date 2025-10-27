26ab4543da5d05bd22bbd27319f5870e2640d627
Nefari0uss <nnefari0uss@gmail.com>
2025-10-26 23:17:35 -0400

    Add a Prettier config, update cspell words, and extensions list for VS Code.

---
+++ .vscode/prettier.config.mjs
@@ -0,0 +1,15 @@
+/**
+ * Prettier configuration object.
+ * @see https://prettier.io/docs/configuration
+ * @type {
+ * import("prettier").Config
+ * }
+ */
+const config = {
+  $schema: "https://json.schemastore.org/prettierrc",
+  printWidth: 120,
+  proseWrap: "preserve",
+  singleQuote: false,
+};
+
+export default config;
